6108b41a21595fcd22f4210b7ebea02df388f47e
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2025-01-02 19:53:35 +0800

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import person3 from "../assets/person3.png";
+import person3 from "../assets/nfttrump1.png";
 
 const About = () => {
   return (
@@ -19,19 +19,10 @@ const About = () => {
         <article className=" flex flex-col gap-6 w-full">
           <h1 className="font-Kavoon text-6xl md:text-7xl break-all">
             <span className="text-white  ">About </span>
-            <span className="text-yellow-500  ">$HARAMPEPE </span>
+            <span className="text-yellow-500  ">$TRUMPIUS </span>
           </h1>
           <div className=" text-white text-[16px] font-normal leading-loose tracking-wide">
-            Introducing Haram Pepe, the craziest crossover of memes and crypto!
-            In a wild corner of the internet, where Harambe and Pepe hold court,
-            this project has burst onto the scene with a mix of laughter and
-            digital riches. It's like watching Harambe do the crypto cha-cha
-            with Pepe, leaving everyone in stitches and counting their
-            HaramCoins. Join the Haram Pepe revolution and witness the memetic
-            madness unfold as Harambe and Pepe take the crypto world by storm.
-            Get ready to laugh, hodl, and meme your way to the moon with Haram
-            Pepe! It's meme-tastic, crypto-cool, and guaranteed to leave you
-            shouting, "Harambe Pepe, take my money!"
+            ALX was the one who created the Kekius profile for Elon and recommended him to use it. Now, he's hinting that Trump will do the same.
           </div>
           <a
             href=""
@@ -39,11 +30,11 @@ const About = () => {
             className="w-[238px] h-[54px] font-Kavoon px-8 py-4 bg-white text-green-700 rounded shadow  border border-lime-400  justify-center items-center gap-[10px] inline-flex"
           >
             <a
-              href="https://app.uniswap.org/#/swap?outputCurrency=0x4B5707F91fabBF04C2A41E892B0A07E3aBaC75df"
+              href=""
               target="_blank"
               className=" text-[16px] leading-snug uppercase"
             >
-              buy on uniswap
+              buy on Pancakeswap
             </a>
           </a>
         </article>
